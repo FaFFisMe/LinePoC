16c31a8ad6266e8cb548598c00050df54191c6df
Paul <paul@Pauls-MacBook-Air.local>
2024-09-04 09:56:07 +0700

    basic code

---
+++ index.js
@@ -0,0 +1,38 @@
+// This code is for Line OA Poc
+
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const line = require('@line/bot-sdk');
+
+const config ={
+    channelAccessToken: process.env.token,
+    channelSecret: process.env.channel
+}
+
+app.post('/webhook', line.middleware(config), (req,res) => {
+    Promise
+        .all([
+            req.body.events.map(handleEvents)
+        ])
+        .then((result) => res.json(result))
+})
+
+function handleEvents(event){
+
+    console.log(event);
+
+    return client.replyMessage(event.replyToken,[
+        {
+            "type": "text",
+            "text": JSON.stringify(event)
+        }
+    ])
+}
+
+app.get('/',(req, res) =>{
+    res.send('Hello World')
+})
+
+app.listen(8080, () => console.log('running in port 8080'))
